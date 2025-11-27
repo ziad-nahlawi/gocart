ec8300293e12584dc75518ff912c78c6185e2f69
ziad-nahlawi <181183831+ziad-nahlawi@users.noreply.github.com>
2025-11-27 20:51:13 +0300

    added ath to vist store page

--- app/store/layout.jsx
+++ app/store/layout.jsx
@@ -1,4 +1,5 @@
 import StoreLayout from "@/components/store/StoreLayout";
+import { SignedIn, SignedOut, SignIn } from '@clerk/nextjs'
 
 export const metadata = {
     title: "GoCart. - Store Dashboard",
@@ -9,9 +10,16 @@ export default function RootAdminLayout({ children }) {
 
     return (
         <>
-            <StoreLayout>
-                {children}
-            </StoreLayout>
+            <SignedIn>
+                <StoreLayout>
+                    {children}
+                </StoreLayout>
+            </SignedIn>
+            <SignedOut>
+                <div className="min-h-screen flex items-center justify-center">
+                    <SignIn fallbackRedirectUrl="/store/" routing="hash" />
+                </div>
+            </SignedOut>
         </>
     );
 }
