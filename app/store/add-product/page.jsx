38bf5e11b72495690f27e3f61f5232b28a577821
ziad-nahlawi <181183831+ziad-nahlawi@users.noreply.github.com>
2025-11-28 01:01:13 +0300

    add product funcitonality

--- app/store/add-product/page.jsx
+++ app/store/add-product/page.jsx
@@ -1,5 +1,7 @@
 'use client'
 import { assets } from "@/assets/assets"
+import { useAuth } from "@clerk/nextjs"
+import axios from "axios"
 import Image from "next/image"
 import { useState } from "react"
 import { toast } from "react-hot-toast"
@@ -18,6 +20,8 @@ export default function StoreAddProduct() {
     })
     const [loading, setLoading] = useState(false)
 
+    const { getToken } = useAuth()
+
 
     const onChangeHandler = (e) => {
         setProductInfo({ ...productInfo, [e.target.name]: e.target.value })
@@ -25,8 +29,45 @@ export default function StoreAddProduct() {
 
     const onSubmitHandler = async (e) => {
         e.preventDefault()
-        // Logic to add a product
-        
+        try {
+            // if no images are uploaded then return
+            if (!images[1] && !images[2] && !images[3] && !images[4]) {
+                return toast.error('Please upload at least one image')
+            }
+            setLoading(true)
+
+            const formData = new FormData()
+            formData.append('name', productInfo.name)
+            formData.append('description', productInfo.description)
+            formData.append('mrp', productInfo.mrp)
+            formData.append('price', productInfo.price)
+            formData.append('category', productInfo.category)
+
+            // Adding Images to FormData
+            Object.keys(images).forEach((key) => {
+                images[key] && formData.append('images', images[key])
+            })
+
+            const token = await getToken()
+            const { data } = await axios.post('/api/store/product', formData, { headers: { Authorization: `Bearer ${token}` } })
+            toast.success(data.message)
+
+            // reset form
+            setProductInfo({
+                name: "",
+                description: "",
+                mrp: 0,
+                price: 0,
+                category: "",
+            })
+            // reset images
+            setImages({ 1: null, 2: null, 3: null, 4: null })
+        } catch (error) {
+            toast.error(error?.response?.data?.error || error.message)
+        }
+        finally {
+            setLoading(false)
+        }
     }
 
 
