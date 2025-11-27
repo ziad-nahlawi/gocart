6ef0ec368625275629ba71a235e2ca3aa38717ba
ziad-nahlawi <181183831+ziad-nahlawi@users.noreply.github.com>
2025-11-28 02:57:41 +0300

    manage products page

--- app/store/manage-product/page.jsx
+++ app/store/manage-product/page.jsx
@@ -4,8 +4,13 @@ import { toast } from "react-hot-toast"
 import Image from "next/image"
 import Loading from "@/components/Loading"
 import { productDummyData } from "@/assets/assets"
+import { useAuth, useUser } from "@clerk/nextjs"
+import axios from "axios"
 
 export default function StoreManageProducts() {
+
+    const { getToken } = useAuth()
+    const { user } = useUser()
 
     const currency = process.env.NEXT_PUBLIC_CURRENCY_SYMBOL || '$'
 
@@ -13,7 +18,13 @@ export default function StoreManageProducts() {
     const [products, setProducts] = useState([])
 
     const fetchProducts = async () => {
-        setProducts(productDummyData)
+        try {
+            const token = await getToken()
+            const { data } = await axios.get('/api/store/product', { headers: { Authorization: `Bearer ${token}` } })
+            setProducts(data.products.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt)))
+        } catch (error) {
+            toast.error(error?.response?.data?.error || error.message)
+        }
         setLoading(false)
     }
 
@@ -24,8 +35,10 @@ export default function StoreManageProducts() {
     }
 
     useEffect(() => {
+        if (user) {
             fetchProducts()
-    }, [])
+        }
+    }, [user])
 
     if (loading) return <Loading />
 
