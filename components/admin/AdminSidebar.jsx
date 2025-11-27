7946c18bb1e55facb08a635e6902375ad873a889
ziad-nahlawi <181183831+ziad-nahlawi@users.noreply.github.com>
2025-11-27 20:40:27 +0300

    fixed logout by addding user to the return at adminsidebar

--- components/admin/AdminSidebar.jsx
+++ components/admin/AdminSidebar.jsx
@@ -20,7 +20,7 @@ const AdminSidebar = () => {
         { name: 'Coupons', href: '/admin/coupons', icon: TicketPercentIcon },
     ]
 
-    return (
+    return user && (
         <div className="inline-flex h-full flex-col gap-5 border-r border-slate-200 sm:min-w-60">
             <div className="flex flex-col gap-3 justify-center items-center pt-8 max-sm:hidden">
                 <Image className="w-14 h-14 rounded-full" src={user.imageUrl} alt="" width={80} height={80} />
