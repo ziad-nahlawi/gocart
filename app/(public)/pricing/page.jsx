d0e12750719116a7ff4e464a6c1838c73b653d39
ziad-nahlawi <181183831+ziad-nahlawi@users.noreply.github.com>
2025-11-28 21:10:09 +0300

    showing pricing table on become member page

--- app/(public)/pricing/page.jsx
+++ app/(public)/pricing/page.jsx
@@ -1,9 +1,9 @@
+import { PricingTable } from "@clerk/nextjs"
 
 export default function PricingPage() {
     return (
         <div className='mx-auto max-w-[700px] my-28'>
-            {/* Pricing Table */}
-
+            <PricingTable></PricingTable>
         </div>
     )
 }
