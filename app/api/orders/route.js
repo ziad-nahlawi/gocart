c073f2701c935649282aa6426e0c8879990a346b
ziad-nahlawi <181183831+ziad-nahlawi@users.noreply.github.com>
2025-11-29 00:33:06 +0300

    added placing order functionality

---
+++ app/api/orders/route.js
@@ -0,0 +1,136 @@
+import prisma from "@/lib/prisma";
+import { getAuth } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+
+export async function POST(request) {
+    try {
+        const { userId, has } = getAuth(request)
+        if (!userId) {
+            return NextResponse.json({ error: "not authorized" }, { status: 401 });
+        }
+        const { addressId, items, couponCode, paymentMethod } = await request.json()
+
+        // Check if all required fields are present
+        if (!addressId || !paymentMethod || !items || !Array.isArray(items) || items.length === 0) {
+            return NextResponse.json({ error: "missing order detials." }, { status: 401 });
+        }
+
+        let coupon = null;
+
+        if (couponCode) {
+            coupon = await prisma.coupon.findUnique({
+                where: { code: couponCode }
+            })
+            if (!coupon) {
+                return NextResponse.json({ error: "Coupon not found" }, { status: 404 })
+            }
+        }
+
+        // Check if coupon is applicable for new users
+        if (couponCode && coupon.forNewUser) {
+            const userorders = await prisma.order.findMany({ where: { userId } })
+            if (userorders.length > 0) {
+                return NextResponse.json({ error: "Coupon Valid for new users" }, { status: 400 })
+            }
+        }
+
+        const hasPlusPlan = has({ plan: 'plus' })
+
+        // Check if coupon is applicable for members
+        if (couponCode && coupon.forMember) {
+            if (!hasPlusPlan) {
+                return NextResponse.json({ error: "Coupon Valid for members only" }, { status: 400 })
+            }
+        }
+
+        // Group orders by storeId using a Map
+        const ordersByStore = new Map()
+
+        for (const item of items) {
+            const product = await prisma.product.findUnique({ where: { id: item.id } })
+            const storeId = product.storeId
+            if (!ordersByStore.has(storeId)) {
+                ordersByStore.set(storeId, [])
+            }
+            ordersByStore.get(storeId).push({ ...item, price: product.price })
+        }
+
+        let orderIds = [];
+        let fullAmount = 0;
+
+        let isShippingFeeAdded = false
+
+        // Create orders for each seller
+        for (const [storeId, sellerItems] of ordersByStore.entries()) {
+            let total = sellerItems.reduce((acc, item) => acc + (item.price * item.quantity), 0)
+
+            if (couponCode) {
+                total -= (total * coupon.discount) / 100;
+            }
+            if (!isPlusMember && !isShippingFeeAdded) {
+                total += 5;
+                isShippingFeeAdded = true
+            }
+
+            fullAmount += parseFloat(total.toFixed(2))
+
+            const order = await prisma.order.create({
+                data: {
+                    userId,
+                    storeId,
+                    addressId,
+                    total: parseFloat(total.toFixed(2)),
+                    paymentMethod,
+                    isCouponUsed: coupon ? true : false,
+                    coupon: coupon ? coupon : {},
+                    orderItems: {
+                        create: sellerItems.map(item => ({
+                            productId: item.id,
+                            quantity: item.quantity,
+                            price: item.price
+                        }))
+                    }
+                }
+            })
+            orderIds.push(order.id)
+        }
+
+        // clear the cart
+        await prisma.user.update({
+            where: { id: userId },
+            data: { cart: {} }
+        })
+
+        return NextResponse.json({ message: 'Orders Placed Successfully' })
+
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error.code || error.message }, { status: 400 })
+    }
+}
+
+// Get all orders for a user
+export async function GET(request) {
+    try {
+        const { userId } = getAuth(request)
+        const orders = await prisma.order.findMany({
+            where: {
+                userId, OR: [
+                    { paymentMethod: paymentMethod.COD },
+                    { AND: [{ paymentMethod: paymentMethod.STRIPE }, { isPaid: true }] }
+                ]
+            },
+            include: {
+                orderItems: { include: { product: true } },
+                address: true
+            },
+            orderBy: { createdAt: 'desc' }
+        })
+
+        return NextResponse.json({ orders })
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ error: error.message }, { status: 400 })
+    }
+}
--- components/OrderSummary.jsx
+++ components/OrderSummary.jsx
@@ -1,16 +1,18 @@
 import { PlusIcon, SquarePenIcon, XIcon } from 'lucide-react';
 import React, { useState } from 'react'
 import AddressModal from './AddressModal';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import toast from 'react-hot-toast';
 import { useRouter } from 'next/navigation';
 import { Protect, useAuth, useUser } from '@clerk/nextjs';
 import axios from 'axios';
+import { fetchCart } from '@/lib/features/cart/cartSlice';
 
 const OrderSummary = ({ totalPrice, items }) => {
 
     const { user } = useUser()
     const { getToken } = useAuth()
+    const dispatch = useDispatch()
     const currency = process.env.NEXT_PUBLIC_CURRENCY_SYMBOL || '$';
 
     const router = useRouter();
@@ -41,8 +43,43 @@ const OrderSummary = ({ totalPrice, items }) => {
 
     const handlePlaceOrder = async (e) => {
         e.preventDefault();
+        try {
+            if (!user) {
+                return toast('Please login to place an order')
+            }
+            if (!selectedAddress) {
+                return toast('Please select an address')
+            }
+            const token = await getToken()
 
-        router.push('/orders')
+            const orderData = {
+                addressId: selectedAddress.id,
+                items,
+                paymentMethod
+            }
+
+            if (coupon) {
+                orderData.couponCode = coupon.code
+            }
+            // create order
+            const { data } = await axios.post('/api/orders', orderData, {
+                headers: { Authorization: `Bearer ${token}` }
+            })
+
+            if (paymentMethod === 'STRIPE') {
+                window.location.href = data.session.url;
+            } else {
+                toast.success(data.message)
+                router.push('/orders')
+                dispatch(fetchCart({ getToken }))
+            }
+
+        } catch (error) {
+            toast.error(error?.response?.data?.error || error.message)
+        }
+
+
+
     }
 
     return (
