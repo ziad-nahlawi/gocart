f1189896de2e19652128afa1add1a7c9674fbecc
ziad-nahlawi <181183831+ziad-nahlawi@users.noreply.github.com>
2025-11-28 08:40:41 +0300

    add to cart functionality

--- app/(public)/layout.jsx
+++ app/(public)/layout.jsx
@@ -3,16 +3,36 @@ import Banner from "@/components/Banner";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "@/lib/features/product/productSlice";
+import { useAuth, useUser } from "@clerk/nextjs";
+import { fetchCart, uploadCart } from "@/lib/features/cart/cartSlice";
 
 export default function PublicLayout({ children }) {
 
     const dispatch = useDispatch()
+    const { user } = useUser()
+    const { getToken } = useAuth()
+
+    const { cartItems } = useSelector((state) => state.cart)
 
     useEffect(() => {
         dispatch(fetchProducts({}))
     }, [])
+
+    useEffect(() => {
+        if (user) {
+            dispatch(fetchCart({ getToken }))
+        }
+    }, [user])
+
+    useEffect(() => {
+        if (user) {
+            dispatch(uploadCart({ getToken }))
+        }
+    }, [cartItems])
+
+
 
     return (
         <>
