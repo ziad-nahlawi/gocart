493edcbf378662836f8ba5e7a661dd7e402e9f5d
ziad-nahlawi <181183831+ziad-nahlawi@users.noreply.github.com>
2025-11-30 04:58:41 +0300

    fixed typo Products and rating and added real data to the store page as a shopper

--- app/(public)/shop/[username]/page.jsx
+++ app/(public)/shop/[username]/page.jsx
@@ -5,7 +5,8 @@ import { useEffect, useState } from "react"
 import { MailIcon, MapPinIcon } from "lucide-react"
 import Loading from "@/components/Loading"
 import Image from "next/image"
-import { dummyStoreData, productDummyData } from "@/assets/assets"
+import axios from "axios"
+import toast from "react-hot-toast"
 
 export default function StoreShop() {
 
@@ -15,8 +16,13 @@ export default function StoreShop() {
     const [loading, setLoading] = useState(true)
 
     const fetchStoreData = async () => {
-        setStoreInfo(dummyStoreData)
-        setProducts(productDummyData)
+        try {
+            const { data } = await axios.get(`/api/store/data?username=${username}`)
+            setStoreInfo(data.store)
+            setProducts(data.store.Product)
+        } catch (error) {
+            toast.error(error?.response?.data?.error || error.message)
+        }
         setLoading(false)
     }
 
@@ -50,7 +56,7 @@ export default function StoreShop() {
                                 <MailIcon className="w-4 h-4 text-gray-500 mr-2" />
                                 <span>{storeInfo.email}</span>
                             </div>
-                           
+
                         </div>
                     </div>
                 </div>
--- app/api/store/data/route.js
+++ app/api/store/data/route.js
@@ -16,7 +16,7 @@ export async function GET(request) {
         // Get store info and inStock products with ratings
         const store = await prisma.store.findUnique({
             where: { username, isActive: true },
-            include: { products: { include: { ratings: true } } }
+            include: { Product: { include: { rating: true } } }
         });
 
         if (!store) {
